9e21a9d30d8458f547a7cd2bd953621ec53f4e9d
jolucode <jolubere.92@gmail.com>
2025-12-06 18:36:32 -0500

    chore: first commit

---
+++ src/main/java/com/customer/service/CustomerServiceApplication.java
@@ -0,0 +1,15 @@
+package com.customer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CustomerServiceApplication.class, args);
+		//System.out.println("Hola Mundo");
+	}
+
+}
